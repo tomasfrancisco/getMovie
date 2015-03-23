43920fbfb8284702699e861a6aea98a837952939
gdsneves <gdsneves@gmail.com>
2015-03-23 15:35:05 +0000

    add home and index

--- client/views/auth/auth.js
+++ client/views/auth/auth.js
@@ -2,7 +2,7 @@
  * Created by tfrancisco on 21/03/15.
  */
 Template.auth.events({
-    'click #auth-btn': function(event) {
+    'click .auth-btn': function(event) {
         console.log("clicked");
         Meteor.call('authTrakt', function(err, result) {
             window.location.href = result;
