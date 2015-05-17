6247e851c653f0aa299b0f28c25c144d05c5dd0f
gdsneves <gdsneves@gmail.com>
2015-05-17 03:04:59 +0100

    teste

--- client/views/recommendations/recommendations.js
+++ client/views/recommendations/recommendations.js
@@ -1,3 +1,21 @@
 /**
  * Created by goncaloneves on 24/04/15.
  */
+Template.movies.helpers({
+    watched: function() {
+        if(Meteor.user()) {
+            if (Session.get("watched")) {
+                console.log("update");
+
+                return Session.get("watched");
+            }
+        }
+    },
+    getWatched: function() {
+        if(Meteor.user()) {
+            Meteor.call('getWatched', Meteor.user().profile.accessToken, Meteor.user().profile.username, "movies", function (err, result) {
+                Session.setPersistent("watched", result);
+            });
+        }
+    }
+});
