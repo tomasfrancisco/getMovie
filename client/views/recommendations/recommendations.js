cba57796d8ef0b2feb65ddacf27d9fd9f0119d97
gdsneves <gdsneves@gmail.com>
2015-05-19 12:40:55 +0100

    UPDATE 19/05 - 12:40 Act: Recommendations/movies

--- client/views/recommendations/recommendations.js
+++ client/views/recommendations/recommendations.js
@@ -7,9 +7,9 @@
 Template.recommendations.helpers({
 
 
-
     moviesRecommendation: function() {
         if(Meteor.user()) {
+
             if (Session.get("moviesRecommendation")) {
                 console.log(Session.get("moviesRecommendation"));
                 return Session.get("moviesRecommendation");
@@ -24,8 +24,24 @@ Template.recommendations.helpers({
                 Session.setPersistent("moviesRecommendation", result);
             });
         }
+
+
     }
 });
 
 
+/*
+*
+* TESTE REPLACE URL
+var movieRec = Session.get("moviesRecommendation");
 
+for(var i = 0; i < movieRec.data.length; i++){
+
+    //var url = movieRec.data[i].trailer;
+    //console.log(url);
+    movieRec.data[i].trailer = movieRec.data[i].trailer.replace('watch?v=', 'embed/');
+    //embedURL =
+    //console.log(embedURL);
+
+}
+console.dir(movieRec);*/
